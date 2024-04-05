2b72c2cb2723af6c8a3036a1379cffbac94f0baf
k-dovan <khanhdovanit@gmail.com>
2024-04-05 14:40:56 +0700

    update requirements

--- chatbot.py
+++ chatbot.py
@@ -57,4 +57,4 @@ def ask_chatchit():
         return Response(answer)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False)
